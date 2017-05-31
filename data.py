eb04bc65cba3f147857852dca34589e3b9ab7b95
Luca <lucav48@gmail.com>
2017-05-31 13:20:07 +0200

    [data.py] completed train and test set

--- data.py
+++ data.py
@@ -5,16 +5,17 @@ from __future__ import print_function
 
 import os
 import numpy as np
+from random import randint
 
 from skimage.io import imread
-
+from sklearn.model_selection import train_test_split
 data_path = 'raw/'
 
 image_rows = 240
 image_cols = 320
+test_percentage = 0.10
 
-
-def create_train_data():
+def create_train_and_test_data():
     train_data_path = os.path.join(data_path, 'train')
     train_data_path_positive = os.path.join(train_data_path, 'positive')
     train_data_path_partial = os.path.join(train_data_path, 'partial')
@@ -60,76 +61,57 @@ def create_train_data():
             print('Done: {0}/{1} images'.format(total, total))
     print('Loading done.')
 
-    ids_array= np.array(ids, dtype=object)
-
-    np.save('imgs_train_16bit.npy', imgs_16bit)
-    np.save('imgs_train_8bit.npy', imgs_8bit)
-    np.save('imgs_mask_train.npy', imgs_mask)
-    np.save('ids_train.npy', ids_array)
-    print('Saving to .npy files done.')
-
-
-def create_test_data():
-    test_data_path = os.path.join(data_path, 'test')
-    test_data_path_positive = os.path.join(test_data_path, 'positive')
-    test_data_path_partial = os.path.join(test_data_path, 'partial')
-    images = os.listdir(test_data_path_positive)
-    images.extend(os.listdir(test_data_path_partial))
-    total = len(images) / 2
-
-    imgs_16bit = np.ndarray((total, image_rows, image_cols), dtype=np.uint16)
-    imgs_8bit = np.ndarray((total, image_rows, image_cols), dtype=np.uint8)
-    ids = []
-
-    counter_16bit = 0
-    counter_8bit = 0
-    print('-' * 30)
-    print('Creating testing images...')
-    print('-' * 30)
-
-    for image_name in images:
-        path = test_data_path_positive
-        if os.path.isfile(os.path.join(test_data_path_partial, image_name)):
-            path = test_data_path_partial
-
-        img = imread(os.path.join(path, image_name), as_grey=True)
-        img = np.array([img])
-        if image_name.split('.')[0].endswith('_8'):
-            imgs_8bit[counter_8bit] = img
-            counter_8bit += 1
-        else:
-            imgs_16bit[counter_16bit] = img
-            counter_16bit += 1
-
-        if counter_8bit % 10 == 0 and counter_16bit % 10 == 0:
-            print('Done: {0}/{1} images'.format(counter_8bit, total))
-        elif counter_8bit == total-1 and counter_16bit == total-1:
-            print('Done: {0}/{1} images'.format(total, total))
-    print('Loading done.')
-
     ids_array = np.array(ids, dtype=object)
+    image_position = np.arange(total)
 
-    np.save('imgs_test_16bit.npy', imgs_16bit)
-    np.save('imgs_test_8bit.npy', imgs_8bit)
-    np.save('ids_test.npy', ids_array)
+    image_position_train, image_position_test =\
+        train_test_split(image_position, test_size=test_percentage, random_state=randint(0, 100))
+
+    ids_train = create_subarray(image_position_train, ids_array)
+    ids_test = create_subarray(image_position_test, ids_array)
+
+    imgs_train_16bit = create_subarray(image_position_train, imgs_16bit)
+    imgs_test_16bit = create_subarray(image_position_test, imgs_16bit)
+
+    imgs_train_8bit = create_subarray(image_position_train, imgs_8bit)
+    imgs_test_8bit = create_subarray(image_position_test, imgs_8bit)
+
+    imgs_mask_train = create_subarray(image_position_train, imgs_mask)
+    imgs_mask_test = create_subarray(image_position_test, imgs_mask)
+
+    np.save('imgs_train_16bit.npy', imgs_train_16bit)
+    np.save('imgs_train_8bit.npy', imgs_train_8bit)
+    np.save('imgs_test_16bit.npy', imgs_test_16bit)
+    np.save('imgs_test_8bit.npy', imgs_test_8bit)
+    np.save('imgs_train_mask.npy', imgs_mask_train)
+    np.save('imgs_test_mask.npy', imgs_mask_test)
+    np.save('ids_train.npy', ids_train)
+    np.save('ids_test.npy', ids_test)
     print('Saving to .npy files done.')
+
+
+def create_subarray(subset_index, total_array):
+    result_array = []
+    for value in subset_index:
+        result_array.append(total_array[value])
+    return result_array
 
 
 def load_train_data():
     imgs_train_16bit = np.load('imgs_train_16bit.npy')
     imgs_train_8bit = np.load('imgs_train_8bit.npy')
-    imgs_train_mask= np.load('imgs_mask_train.npy')
-    imgs_id = np.load('ids_train.npy')
-    return imgs_train_16bit, imgs_train_8bit, imgs_train_mask, imgs_id
+    imgs_train_mask= np.load('imgs_train_mask.npy')
+    imgs_train_id = np.load('ids_train.npy')
+    return imgs_train_16bit, imgs_train_8bit, imgs_train_mask, imgs_train_id
 
 
 def load_test_data():
     imgs_test_16bit = np.load('imgs_test_16bit.npy')
     imgs_test_8bit = np.load('imgs_test_8bit.npy')
-    imgs_id = np.load('ids_test.npy')
-    return imgs_test_16bit, imgs_test_8bit, imgs_id
+    imgs_test_mask = np.load('imgs_test_mask.npy')
+    imgs_test_id = np.load('ids_test.npy')
+    return imgs_test_16bit, imgs_test_8bit, imgs_test_mask, imgs_test_id
 
 
 if __name__ == '__main__':
-    create_train_data()
-    create_test_data()
+    create_train_and_test_data()
